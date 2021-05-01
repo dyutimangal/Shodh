f63a217acc219005e986a5425b02d0166c59a31f
varde2407 <atharvavarde@gmail.com>
2021-05-01 15:59:07 +0530

    first commit

---
+++ simplesocial/groups/models.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.urls import reverse
+from django.db import models
+from django.utils.text import slugify
+
+
+import misaka
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+from django import template
+register = template.Library()
+
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(allow_unicode=True, unique=True)
+    description = models.TextField(blank=True, default='')
+    description_html = models.TextField(editable=False, default='', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        self.description_html = misaka.html(self.description)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+
+
+    class Meta:
+        ordering = ["name"]
